dd0bfbcb2ae4ccaf811be6c5e83d04f137b4e58b
yuliya-gri <yuliya.grigoreva.1986@list.ru>
2022-11-24 16:44:21 +0400

    Commit DZ.HARD

---
+++ DZ.HARD/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if(x > 99)
+{
+
+while (x >= 1000) x = x / 10;
+
+Console.WriteLine("Третья цифра слева");
+Console.WriteLine(x % 10);
+
+}
+else
+{
+    Console.WriteLine("Введено некорректное число");
+}
+    
+
+    
+        
+    
