6bba4e9c3ca5306eb2a8d68c0a227683e0b474c2
yuliya-gri <yuliya.grigoreva.1986@list.ru>
2022-11-24 15:31:10 +0400

    commit DZ.1

---
+++ DZ.1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+ if(num1 > num2)
+ {
+    Console.WriteLine("Первое число больше второго");
+ }
+ if(num1 < num2)
+ {
+    Console.WriteLine("Первое число меньше второго");
+ }
+
+ else
+{
+    Console.WriteLine("Оба числа равны");
+}
